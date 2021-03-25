763717cc30f8e2ad9c91ab38174af8d8a9625344
unknown <b07902042@ntu.edu.tw>
2021-03-26 02:46:50 +0800

    hw3

--- hw3/own/src/components/Footer.js
+++ hw3/own/src/components/Footer.js
@@ -3,14 +3,31 @@ import React, { Component } from "react";
 class TodoFooter extends Component{
     constructor(props){
         super(props);
-        this.state = {realnum : 0};
+        this.state = {};
+    }
+    handleFilter = (f)=>{
+        this.props.changefilter(f);
+    }
+    handleClear = () => {
+        this.props.changeclear(1);
+        //this.props.changeclear(0);
     }
     render(){
+        var hide="";
+        if((this.props.compNum+this.props.todoNum) ==0) hide="none";
+        var hideb="";
+        if((this.props.compNum) ==0) hideb="none";
         return(
-            <footer class="todo-app__footer" id="todo-footer">
-                <div class="todo-app__total">{this.state.realnum} left</div>
-                <ul class="todo-app__view-buttons">All</ul>
-                <div class="todo-app__clean">clear completed</div>
+            <footer class="todo-app__footer" id="todo-footer" style={{display:hide}}>
+                <div class="todo-app__total">{this.props.todoNum} left</div>
+                <ul class="todo-app__view-buttons">
+                    <button onClick = {()=>this.handleFilter("ALL")}>All</button>
+                    <button onClick = {()=>this.handleFilter("ACT")}>Active</button>
+                    <button onClick = {()=>this.handleFilter("COMP")}>Completed</button>
+                </ul>
+                <div class="todo-app__clean">
+                    <button  style={{display:hideb}} onClick = {()=>this.handleClear()}>Clear completed</button>
+                </div>
             </footer>   
         );
     }
--- hw3/own/src/containers/TodoApp.js
+++ hw3/own/src/containers/TodoApp.js
@@ -5,14 +5,30 @@ import Footer from "../components/Footer";
 class TodoApp extends Component {
     constructor(props){
         super(props);
-        this.state = {todoList : [], todoNum : 0, realNum : 0};
+        this.state = { todoNum : 0 ,compNum:0, filter:"ALL" ,clear:0};
+        this.changetodoNum = this.changetodoNum.bind(this);
+        this.changecompNum = this.changecompNum.bind(this);
+        this.changefilter = this.changefilter.bind(this);
+        this.changeclear = this.changeclear.bind(this);
+    }
+    changetodoNum(num){
+        this.setState({todoNum : num});
+    }
+    changecompNum(num){
+        this.setState({compNum : num});
+    }
+    changefilter(s){
+        this.setState({filter : s});
+    }
+    changeclear(s){
+        this.setState({clear : s});
     }
     render() {
         return (
             <>
                 <Header text="todos" />
-                <Main todoList={this.state.todoList} todoNum = {this.state.todoNum} realNum = {this.state.realNum}/>
-                <Footer/>
+                <Main filter={this.state.filter} clear={this.state.clear} changetodoNum={this.changetodoNum} changecompNum={this.changecompNum} changeclear={this.changeclear}/>
+                <Footer todoNum={this.state.todoNum} compNum={this.state.compNum} changefilter={this.changefilter} changeclear={this.changeclear}/>
             </>
         );
     }
